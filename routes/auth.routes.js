10fb8d133501766009652cdd39ebe0bdcb09e993
Zain Ul Basit <zainulbasit486@gmail.com>
2024-03-06 19:50:27 +0500

    changes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+const auth = require("../Middleware/auth");
 const authControllers = require("../controllers/authControllers");
 
 router.post("/login", authControllers().login);
-router.post("/logout", authControllers().logout);
-router.post("/register", authControllers().register)
+router.post("/logout", auth, authControllers().logout);
+router.post("/register", authControllers().register);
 router.post("/refresh", authControllers().autoLogin);
 
 module.exports = router;
