bef15655ad0ed4918263ca3f7304d4039c96c3b5
Zain Ul Basit <zainulbasit486@gmail.com>
2024-03-06 18:12:10 +0500

    Chnagess

--- controllers/customer-controllers.js
+++ controllers/customer-controllers.js
@@ -1,4 +1,3 @@
-const CustomerPayment = require("../Models/Payment");
 const bcrypt = require("bcrypt");
 const CusDto = require("../Services/CustomerDTO");
 const Joi = require("joi");
@@ -76,7 +75,6 @@ const UpdateCustomer = async (req, res, next) => {
     return createError(res, 500, err.message || err);
   }
 };
-
 const deleteCustomer = async (req, res, next) => {
   const { customerId } = req.body;
 
@@ -191,168 +189,11 @@ const addCustomer = async (req, res, next) => {
   }
 };
 
-// *****************************************************
-// *****************************************************
-const AddNewProperty = async (req, res, next) => {
-  const { id } = req.body;
-  try {
-    // Define the new property and its value to be added
-    let duedate;
-    let returndate;
-    let newValue = new Date();
-    const update = { duedate: newValue, returndate: newValue };
-
-    // Use `updateMany()` to add the new property to all documents in the "items" collection
-    const updateResult = await Customer.findOneAndUpdate({ _id: id }, update, {
-      new: true,
-    });
-    if (!updateResult) res.status(404).json("Customer Not found");
-    else res.status(200).json({ updateResult });
-    console.log("Update Result:", updateResult);
-  } catch (error) {
-    console.error("Error updating documents:", error);
-  }
-};
-// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-//******************************************************
-// working
-//******************************************************
-const updateCustomerTotal = async (req, res, next) => {
-  const customerID = req.params.id;
-  const { curTotal, curDiscount } = req.body;
-  let customer;
-  try {
-    const filter = { _id: customerID };
-    const increments = {
-      total: curTotal,
-      remaining: curTotal,
-      discount: curDiscount,
-    };
-    const update = { $inc: increments };
-    customer = await Customer.updateOne(
-      filter,
-      update,
-      { new: true },
-      (err, data) => {
-        if (data === null) {
-          return res.status(404).json({ message: "Customer not found" });
-        } else {
-          return res.status(200).json({ customer });
-        }
-      }
-    );
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-//******************************************************
-// working
-//******************************************************
-const updateCustomerPayment = async (req, res, next) => {
-  const customerid = req.params.id;
-  const customershop = req.params.id1;
-  const { newPaid } = req.body;
-  let customer;
-  try {
-    const filter = { _id: customerid, shop: customershop };
-    const newRem = newPaid * -1;
-    const increments = {
-      remaining: newRem,
-      paid: newPaid,
-    };
-    const update = { $inc: increments };
-    customer = await Customer.updateOne(
-      filter,
-      update,
-      { new: true },
-      (err, data) => {
-        if (data === null) {
-          return res.status(404).json({ message: "Customer not found" });
-        } else {
-          return res.status(200).json({ customer });
-        }
-      }
-    );
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-//******************************************************
-// working
-//******************************************************
-const addCustomerPayment = async (req, res, next) => {
-  let customerPayment;
-  const { custid, name, cash, accountno, date, description, shop } = req.body;
-  try {
-    customerPayment = new CustomerPayment({
-      custid,
-      name,
-      cash,
-      accountno,
-      date,
-      description,
-      shop,
-    });
-    await customerPayment.save();
-  } catch (err) {
-    console.log(err);
-  }
-
-  if (!customerPayment) {
-    return res.status(500).json({ message: "Unable to Add Company" });
-  }
-  return res.status(201).json({ customerPayment });
-};
-//******************************************************
-// working
-//******************************************************
-const getCustomerPayment = async (req, res, next) => {
-  const { cid, sname, fromdate, todate } = req.body;
-  console.log(req.body);
-  const startDate = new Date(fromdate);
-  const endDate = new Date(todate);
-  let customerPayment;
-  const filter = { custid: cid, shop: sname };
-  try {
-    if (sname === "Admin") {
-      customerPayment = await CustomerPayment.find({
-        custid: cid,
-        date: {
-          $gte: startDate,
-          $lte: endDate,
-        },
-      });
-    } else {
-      customerPayment = await CustomerPayment.find({
-        custid: cid,
-        shop: sname,
-        date: {
-          $gte: startDate,
-          $lte: endDate,
-        },
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-
-  if (!customerPayment) {
-    return res.status(500).json({ message: "Unable to Add Company" });
-  }
-  return res.status(201).json(customerPayment);
-};
-
 module.exports = {
   getBranchCustomers,
   getAllCustomers,
   UpdateCustomer,
   deleteCustomer,
   addCustomer,
-  AddNewProperty,
-  updateCustomerTotal,
-  addCustomerPayment,
-  getCustomerPayment,
-  updateCustomerPayment,
   CheckCustomers,
 };
