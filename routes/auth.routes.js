799695c16672cdea1b59e7641a97ed983518c6d7
Zain Ul Basit <zainulbasit486@gmail.com>
2024-03-06 18:28:33 +0500

    changes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ const authControllers = require("../controllers/authControllers");
 
 router.post("/login", authControllers().login);
 router.post("/logout", authControllers().logout);
+router.post("/register", authControllers().register)
 router.post("/refresh", authControllers().autoLogin);
 
 module.exports = router;
