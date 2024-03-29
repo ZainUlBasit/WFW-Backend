688580b29db4bae68f5ec45138a336ddb64347d3
ZainUlBasit <zainulbasit486@gmail.com>
2024-03-29 19:37:11 +0500

    changes

--- server.js
+++ server.js
@@ -32,7 +32,13 @@ const TransactionkRoutes = require("./routes/transaction-route");
 global.rootDirectory = path.resolve(__dirname);
 
 const corsOptions = {
-  origin: ["https://www.irshadcartondealer.com"],
+  origin: [
+    "http://localhost:3000",
+    "https://wfw-expert-system.vercel.app",
+    "https://www.irshadcartondealer.com",
+    "http://localhost:5174",
+    "http://localhost:5173",
+  ],
   credentials: true,
 };
 
