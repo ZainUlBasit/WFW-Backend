e93efd425c5c76fef2f39ad027d9702ea95c4ac5
ZainUlBasit <zainulbasit486@gmail.com>
2024-04-26 14:58:34 +0500

    changes

--- server.js
+++ server.js
@@ -33,14 +33,17 @@ global.rootDirectory = path.resolve(__dirname);
 
 const corsOptions = {
   origin: [
-    "https://www.irshadcartondealer.com", // Add your frontend URL here
+    "http://localhost:3000",
+    "https://wfw-expert-system.vercel.app",
+    "https://www.irshadcartondealer.com",
+    "http://localhost:5174",
+    "http://localhost:5173",
   ],
   credentials: true,
 };
 
-// app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json());
